189fd9e1258e311eb4d29effa2c8532da9288ac1
arunbababa <hatuki.1.gzs@icloud.com>
2025-03-16 14:32:50 +0900

    一旦この状態でdeployしてCIが正常か確認する

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Cards from './components/cards.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="cards/:id" element={<Cards />} />
+        </Routes>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
